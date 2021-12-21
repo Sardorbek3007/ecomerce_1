0a22643682594a465cc23303e2c9bd24e7da7652
Sardorbek3007 <ergashbekovsardorbek@gmail.com>
2021-12-21 11:36:19 +0500

    deb

--- mebel/settings.py
+++ mebel/settings.py
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS =['mebel/static']
 
 MEDIA_URL = '/media/'
